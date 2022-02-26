b51cbb42fa4803cff6e2fe12ed2be67522af6fee
Chris Hile <chile@student.fullsail.edu>
2022-02-26 10:24:32 -0500

    updates

--- js/data.js
+++ js/data.js
@@ -1,52 +1,47 @@
 class Data {
-    constructor(){
-        this.fetchData();
-        this.results;
-    }
+  constructor() {
+    this.fetchData();
+    this.results;
+  }
 
-    fetchData() {
-        fetch("blog.json")
-        .then((response) => response.json())
+  fetchData() {
+    fetch("blog.json")
+      .then((response) => response.json())
       .then((responseAsJson) => {
         const data = responseAsJson;
-        
+
         this.view(data);
-    })
+      })
       .catch((error) => {
         console.log("An Error Occurred:", error);
       });
+  }
 
-    }
-
-    view(data){
-        
-
-        data.posts.forEach(element => {
-            
-            const recent = document.querySelector("#recent");
-            let recentPost = document.createElement("li");
-            recent.append(recentPost);
-        recentPost.innerHTML = `
+  view(data) {
+    
+    data.posts.forEach((element) => {
+      const recent = document.querySelector("#recent");
+      let recentPost = document.createElement("li");
+      recent.append(recentPost);
+      recentPost.innerHTML = `
            <a class="postLink" href=${element.link}> ${element.title} </a>
            <br>
             ${element.date}
-        `
-        });
+        `;
+    });
 
-        if (window.location.pathname == "/index.html") {
-    
-            let file = data.posts[0].post
-            fetch(file)
-            .then(response => response.text()
-            .then(text => {let postDetail = text;
-                data.posts[0].post = postDetail;
+    if (window.location.pathname == "/index.html" || window.location.pathname == "/") {
+      let file = data.posts[0].post;
+      fetch(file).then((response) =>
+        response.text().then((text) => {
+          let postDetail = text;
+          data.posts[0].post = postDetail;
 
+          const post = document.querySelector(".post");
+          let postData = document.createElement("secion");
+          post.append(postData);
 
-            const post = document.querySelector(".post");
-        let postData = document.createElement("secion");
-        post.append(postData);
-       
-        postData.innerHTML = `
+          postData.innerHTML = `
         <button class="postCat rounded-pill">${data.posts[0].category}</button>
         <h3>${data.posts[0].title}</h3>
             <div class="row">
@@ -62,26 +57,25 @@ class Data {
                 </div>
             </div>
         `;
-    })
-    );
-        }
-        else {
+        })
+      );
+    } else { 
+      data.posts.forEach((element) => {
+        if (
+          window.location.pathname == "/" + element.link ||
+          window.location.pathname == "/blog.html"
+        ) {
+          let file = element.post;
+          fetch(file).then((response) =>
+            response.text().then((text) => {
+              let postDetail = text;
+              element.post = postDetail;
 
-        data.posts.forEach(element => {     
-        
-            if (window.location.pathname == "/" + element.link || window.location.pathname == "/blog.html") {
+              const post = document.querySelector(".post");
+              let postData = document.createElement("secion");
+              post.append(postData);
 
-                let file = element.post
-                fetch(file)
-                .then(response => response.text()
-                .then(text => {let postDetail = text;
-                    element.post = postDetail;
-
-                const post = document.querySelector(".post");
-                let postData = document.createElement("secion");
-                post.append(postData);
-               
-                postData.innerHTML = `
+              postData.innerHTML = `
                 <button class="postCat rounded-pill">${element.category}</button>
                 <h3>${element.title}</h3>
                     <div class="row">
@@ -98,13 +92,9 @@ class Data {
                     </div>
                 `;
             })
-            );
-            }
-        });
+          );
         }
-
+      });
     }
-    
+  }
 }
-     
-    
